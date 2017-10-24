f0d5c4afcdf4c6237a0d78710fac541a494c2b86
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-10-24 11:31:43 +0200

    docs

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 This module contains the tool of cs.at.gipuzkoairekia
 """
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 
 def read(*rnames):
@@ -32,39 +33,52 @@ long_description = (
     'Download\n'
     '********\n')
 
-tests_require = ['zope.testing']
-
-setup(name='cs.at.gipuzkoairekia',
-      version=version,
-      description="An add-on for Plone to expose the contents of Gipuzkoa Irekia, an open government platform",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        'Framework :: Plone',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+setup(
+    name='cs.at.gipuzkoairekia',
+    version=version,
+    description="An add-on for Plone to expose the contents of Gipuzkoa Irekia, an open government platform",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='',
+    author='Mikel Larreategi',
+    author_email='mlarreategi@codesyntax.com',
+    url='https://github.com/codesyntax/cs.at.gipuzkoairekia',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['cs', 'at'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'Plone <= 4.99',
+        'Products.Archetypes',
+        'archetypes.schemaextender',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            # Plone KGS does not use this version, because it would break
+            # Remove if your package shall be part of coredev.
+            # plone_coredev tests as of 2016-04-01.
+            'plone.testing>=5.0.0',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cs', 'at'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='cs.gipuzkoairekia.tests.test_docs.test_suite',
-      entry_points="""
-      # -*- entry_points -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+    },
+    entry_points="""
+    # -*- entry_points -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins=["ZopeSkel"],
+    )
