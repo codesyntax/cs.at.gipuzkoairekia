1be319a7ed4fde233fdc747018b1c8aea0573061
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-01-30 10:17:06 +0100

    fix test setup

--- cs/at/gipuzkoairekia/interfaces/__init__.py
+++ cs/at/gipuzkoairekia/interfaces/__init__.py
@@ -2,3 +2,4 @@
 from .transparencysection import ITransparencySection
 from .gipuzkoairekia import IGipuzkoaIrekiaFolder
 from .opendatafolder import IOpenDataFolder
+from .browserlayer import ICsAtGipuzkoaIrekia
---
+++ cs/at/gipuzkoairekia/interfaces/browserlayer.py
@@ -0,0 +1,5 @@
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IICsAtGipuzkoaIrekia(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
