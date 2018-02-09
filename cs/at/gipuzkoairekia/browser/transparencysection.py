f2ed12a911c7ec7b5c675c610e56aafc71770cd3
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-02-09 11:56:00 +0100

    Revert "fix attributename"
    This reverts commit 702017cc5dc440461c7e54331c4122a30bfded78.

--- cs/at/gipuzkoairekia/browser/transparencysection.py
+++ cs/at/gipuzkoairekia/browser/transparencysection.py
@@ -161,7 +161,7 @@ class TransparencySectionView(BrowserView):
         )
         item['text'] = self.parse_content(item.get('content'), language)
         item['modified'] = self.convert_date(item.get('modifiedDate'))
-        item['created'] = self.convert_date(item.get('createdDate'))
+        item['created'] = self.convert_date(item.get('createDate'))
         return item
 
     def parse_title(self, content, language):
